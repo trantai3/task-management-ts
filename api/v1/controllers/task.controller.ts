0a4544a115086fcb1ad23017bb587cbb6d0d1d27
trantai3 <trantai09032003@gmail.com>
2024-10-03 20:27:15 +0700

    status filter

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,9 +2,18 @@ import { Request, Response } from "express"
 import Task from "../models/task.model"
 
 export const index = async (req: Request, res: Response): Promise<void> => {
-    const tasks = await Task.find({
-        deleted: "false"
-    })
+    interface Find {
+        deleted: boolean,
+        status?: string
+    }
+    const find: Find = {
+        deleted: false
+    }
+
+    if (req.query.status) {
+        find.status = req.query.status.toString() // chỉ cần dùng find["status"] cho ngắn gọn
+    }
+    const tasks = await Task.find(find)
 
     res.json(tasks)
 }
