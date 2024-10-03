3ec2b469420590264a0f87525c95693122923171
trantai3 <trantai09032003@gmail.com>
2024-10-03 21:09:49 +0700

    search

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express"
 import Task from "../models/task.model"
 import paginationHelper from "../../../helpers/pagination"
+import searchHelper from "../../../helpers/search"
 
 export const index = async (req: Request, res: Response): Promise<void> => {
     // Find
     interface Find {
         deleted: boolean,
-        status?: string
+        status?: string,
+        title?: RegExp
     }
     const find: Find = {
         deleted: false
@@ -16,6 +18,14 @@ export const index = async (req: Request, res: Response): Promise<void> => {
         find.status = req.query.status.toString() // chỉ cần dùng find["status"] cho ngắn gọn
     }
     // End find
+
+    // Search
+    let objectSearch = searchHelper(req.query)
+
+    if (req.query.keyword) {
+        find.title = objectSearch.regex
+    }
+    // End search
 
     // Sort
     const sort = {}
