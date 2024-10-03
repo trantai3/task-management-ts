02c4fdd35f952f2a29a31040f8ae7718b6617dc0
trantai3 <trantai09032003@gmail.com>
2024-10-03 20:37:56 +0700

    sort by criteria

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express"
 import Task from "../models/task.model"
 
 export const index = async (req: Request, res: Response): Promise<void> => {
+    // Find
     interface Find {
         deleted: boolean,
         status?: string
@@ -13,7 +14,17 @@ export const index = async (req: Request, res: Response): Promise<void> => {
     if (req.query.status) {
         find.status = req.query.status.toString() // chỉ cần dùng find["status"] cho ngắn gọn
     }
-    const tasks = await Task.find(find)
+    // End find
+
+    // Sort
+    const sort = {}
+
+    if (req.query.sortKey && req.query.sortValue) {
+        const sortKey = req.query.sortKey.toString()
+        sort[sortKey] = req.query.sortValue 
+    }
+    // End sort 
+    const tasks = await Task.find(find).sort(sort)
 
     res.json(tasks)
 }
