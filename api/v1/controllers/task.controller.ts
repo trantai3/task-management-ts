4b025085346c904a4b1bf1574ac579c534135119
trantai3 <trantai09032003@gmail.com>
2024-10-04 20:39:11 +0700

    install cors

--- api/v1/controllers/task.controller.ts
+++ api/v1/controllers/task.controller.ts
@@ -86,12 +86,16 @@ export const changeStatus = async (req: Request, res: Response): Promise<void> =
 
 export const changeMulti = async (req: Request, res: Response): Promise<void> => {
     try {
+        enum Key {
+            STATUS = "status",
+            DELETE = "delete"
+        }
         const ids: string[] = req.body.ids
         const key: string = req.body.key
         const value: string = req.body.value
 
         switch (key) {
-            case "status":
+            case Key.STATUS:
                 await Task.updateMany({
                     _id: { $in: ids }
                 }, {
@@ -102,7 +106,7 @@ export const changeMulti = async (req: Request, res: Response): Promise<void> =>
                     message: "Cập nhật trạng thái thành công!"
                 })
                 break
-            case "delete":
+            case Key.DELETE:
                 await Task.updateMany({
                     _id: { $in: ids }
                 }, {
@@ -156,6 +160,27 @@ export const edit = async (req: Request, res: Response): Promise<void> => {
         res.json({
             code: 200,
             message: "Cập nhật thành công!"
+        })
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: "Lỗi!"
+        })
+    }
+}
+
+export const deleteTask = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const id = req.params.id
+
+        await Task.updateOne({ _id: id }, {
+            deleted: true,
+            deletedAt: new Date()
+        })
+
+        res.json({
+            code: 200,
+            message: "Xóa thành công!"
         })
     } catch (error) {
         res.json({
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express } from "express"
 import dotenv from "dotenv"
+import cors from "cors"
 import * as database from "./config/database"
 import mainV1Routes from "./api/v1/routes/index.route"
 
@@ -8,8 +9,17 @@ database.connect()
 const app: Express = express()
 const port: number | string = process.env.PORT || 3000
 
+// const corsOptions = {
+//     origin: 'http://example.com',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions))
+
+app.use(cors()) // cho phép front end có thể lấy được data của backend
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
 mainV1Routes(app)
 
 app.listen(port, () => {
